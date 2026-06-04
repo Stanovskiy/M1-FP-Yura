3b044b8a940a3e3b68ba0489b8b1da7d6b93de5a
Uriel <stanovskiyyura@gmail.com>
2026-06-04 16:36:21 +0300

    Reload

--- src/main/java/ua/com/javarush/j4/Main.java
+++ src/main/java/ua/com/javarush/j4/Main.java
@@ -1,5 +1,9 @@
 package ua.com.javarush.j4;
 
+import java.io.IOException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /**
  * Точка входу криптоаналізатора шифру Цезаря.
  *
@@ -11,14 +15,33 @@ public class Main {
 
         // TODO: реалізуй CLI шифру Цезаря. Дивись MainTest.
 
+        FileService fileService = new FileService();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter command (ENCRYPT/DECRYPT/BRUTE_FORCE):");
+        String command = scanner.nextLine();
+        System.out.println("Enter input file path:");
+        String filePath = scanner.nextLine();
 
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
+        try {
+            if ("ENCRYPT".equalsIgnoreCase(command) || "DECRYPT".equalsIgnoreCase(command)) {
+                System.out.println("Enter key:");
+                int key = scanner.nextInt();
+                if ("ENCRYPT".equalsIgnoreCase(command)) {
+                    fileService.encryptFile(filePath, key);
+                } else {
+                    fileService.decryptFile(filePath, key);
+                }
+            } else if ("BRUTE_FORCE".equalsIgnoreCase(command)) {
+                BruteForce bruteForces = new BruteForce();
+                bruteForces.bruteForce(filePath);
+            } else {
+                System.out.println("Invalid command.");
+            }
+        } catch (InputMismatchException | IOException e) {
+            e.printStackTrace();
+        } finally {
+            scanner.close();
+        }
 
     }
 }
